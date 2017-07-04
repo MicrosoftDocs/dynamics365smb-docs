ea01429b136ec2df037140a67dd5a10af45309cb
Søren Groes-Petersen <sgroespe@microsoft.com>
2017-07-04 21:56:52 +0200

    added \ in build file

--- .openpublishing.build.ps1
+++ .openpublishing.build.ps1
@@ -8,7 +8,7 @@ $errorActionPreference = 'Stop'
 # Step-1: Download buildcore script to local
 echo "download build core script to local with source url: $buildCorePowershellUrl"
 $repositoryRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$buildCorePowershellDestination = "$repositoryRoot.openpublishing.buildcore.ps1"
+$buildCorePowershellDestination = "$repositoryRoot\.openpublishing.buildcore.ps1"
 Invoke-WebRequest $buildCorePowershellUrl -OutFile "$buildCorePowershellDestination"
 
 # Step-2: Run build core
